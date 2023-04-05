2c15cea763fc77b771c7deecad8bb0a2f2e2c61f
Shreyansh911 <47926870+Shreyansh911@users.noreply.github.com>
2023-04-05 18:31:55 +0530

    Chemical Reactions

--- Assets/Scripts/ParticleCatcher.cs
+++ Assets/Scripts/ParticleCatcher.cs
@@ -6,6 +6,21 @@ public class ParticleCatcher : MonoBehaviour
 {
     [SerializeField] GameObject _tube;
 
+    private void Start()
+    {
+
+    }
+
+    public Color GetEndSideColor()
+    {
+        return _tube.GetComponentInParent<TestTube>().EndSideColor;
+    }
+    
+    public Color GetEndTopColor()
+    {
+        return _tube.GetComponentInParent<TestTube>().EndTopColor;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
         //Debug.Log($"Tube: {(other != _tube)} Tag: {(other.tag == "Filler")}");
@@ -13,8 +28,15 @@ public class ParticleCatcher : MonoBehaviour
 
         if (other != _tube && other.CompareTag("Filler"))
         {
+            if (_tube.GetComponentInParent<TestTube>().hasExplosive)
+            {
+
+                return;
+            }
+
             print("Hit and Run: " + other.name);
             other.GetComponentInParent<TestTube>().ChangeVolume(0.0005f);
+            other.GetComponentInParent<TestTube>().ChangeColor(GetEndSideColor(), GetEndTopColor());
         }
     }
 }
--- Assets/Scripts/TestTube.cs
+++ Assets/Scripts/TestTube.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class TestTube : MonoBehaviour
 {
+    public bool hasExplosive;
+
     [SerializeField] float _dropRate; 
     [SerializeField] Renderer _renderer;
     [SerializeField] ParticleSystem _flow;
+
+    public Color EndSideColor, EndTopColor;
 
     // Start is called before the first frame update
     void Start()
@@ -14,10 +19,20 @@ public class TestTube : MonoBehaviour
 
     }
 
+    public Color GetSideColor()
+    {
+        return _renderer.material.GetColor("_SideColor");
+    }
+
+    public Color GetTopColor()
+    { 
+        return _renderer.material.GetColor("_TopColor");
+    }
+
     // Update is called once per frame
     void Update()
     {
-        if ((Vector3.Dot(transform.up, Vector3.down) > 0) && _renderer.material.GetFloat("_Fill") >= 0.2f)
+        if ((Vector3.Dot(transform.up, Vector3.down) > 0) && _renderer.material.GetFloat("_Fill") >= 0.4f)
         {
             ChangeVolume(-_dropRate);
 
@@ -28,7 +43,7 @@ public class TestTube : MonoBehaviour
             _flow.Stop();
         }
 
-        if (_renderer.material.GetFloat("_Fill") < 0.2f)
+        if (_renderer.material.GetFloat("_Fill") < 0.4f)
         {
             _renderer.material.SetFloat("_Fill", 0);
         }
@@ -37,5 +52,15 @@ public class TestTube : MonoBehaviour
     public void ChangeVolume(float volume)
     {
         _renderer.material.SetFloat("_Fill", _renderer.material.GetFloat("_Fill") + volume);
+    }
+
+
+    public void ChangeColor(Color Side, Color Top)
+    {
+        Color newSide = Color.Lerp(GetSideColor(), Side, .01f);
+        _renderer.material.SetColor("_SideColor", newSide);
+        
+        Color newTop = Color.Lerp(GetTopColor(), Top, .01f);
+        _renderer.material.SetColor("_TopColor", newTop);
     }
 }
